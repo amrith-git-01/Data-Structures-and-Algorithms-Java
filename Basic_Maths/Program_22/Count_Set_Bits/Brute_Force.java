98c022bd21ba4a0c8aef78311ae8d1fd90cea6df
Amrith <amrith@axintelligence.ai>
2026-01-04 12:34:11 +0530

    Implemented brute force and optimal approaches for counting set bits in a number

--- Basic_Maths/Program_22/Count_Set_Bits/Brute_Force.java
+++ Basic_Maths/Program_22/Count_Set_Bits/Brute_Force.java
@@ -1,5 +1,22 @@
 package Basic_Maths.Program_22.Count_Set_Bits;
 
+import java.util.Scanner;
+
 public class Brute_Force {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = scanner.nextInt();
+        System.out.println("Number of set bits in " + num + " is: " + countSetBits(num));
+        scanner.close();
+    }
+
+    public static int countSetBits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    }
 }
--- Basic_Maths/Program_22/Count_Set_Bits/Optimal_Approach.java
+++ Basic_Maths/Program_22/Count_Set_Bits/Optimal_Approach.java
@@ -1,5 +1,22 @@
 package Basic_Maths.Program_22.Count_Set_Bits;
 
+import java.util.Scanner;
+
 public class Optimal_Approach {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = scanner.nextInt();
+        System.out.println("The number of set bits in " + num + " is: " + countSetBits(num));
+        scanner.close();
+    }
+
+    public static int countSetBits(int num) {
+        int count = 0;
+        while (num > 0) {
+            num = num & (num - 1);
+            count++;
+        }
+        return count;
+    }
 }
