801947eae183698acc920fcb935f25a5aff489ae
Amrith <selvarajamrith958@gmail.com>
2026-03-25 09:17:43 +0530

    Implement a solution for finding the lower bound index in a sorted array using binary search, with user input handling in Java.

---
+++ BinarySearch/Easy/LowerBound/Solution.java
@@ -0,0 +1,35 @@
+package BinarySearch.Easy.LowerBound;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = scanner.nextInt();
+        System.out.println("Enter the array eleemnts: ");
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Enter the target: ");
+        int target = scanner.nextInt();
+        System.out.println("The lowerbound index is: " + findLowerBound(arr, target));
+        scanner.close();
+    }
+
+    public static int findLowerBound(int arr[], int target) {
+        int ans = arr.length;
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
